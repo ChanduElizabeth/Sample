bb437f893951ebe177d24d900e0c6a81f16624b4
Abhilash Engala <Elizabeth@deep-mac-mini.local>
2019-01-29 19:42:05 +0530

    First Commit

---
+++ FirstFrameworkExp/BasicClass.h
@@ -0,0 +1,17 @@
+//
+//  BasicClass.h
+//  FirstFrameworkExp
+//
+//  Created by Elizabeth on 29/01/19.
+//  Copyright © 2019 tvisha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BasicClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FirstFrameworkExp/BasicClass.m
@@ -0,0 +1,13 @@
+//
+//  BasicClass.m
+//  FirstFrameworkExp
+//
+//  Created by Elizabeth on 29/01/19.
+//  Copyright © 2019 tvisha. All rights reserved.
+//
+
+#import "BasicClass.h"
+
+@implementation BasicClass
+
+@end

7b98fc7596776094a0016a71f783527000e78a20
Abhilash Engala <Elizabeth@deep-mac-mini.local>
2019-01-29 19:37:38 +0530

    Initial Commit

---
+++ FirstFrameworkExp/FirstFrameworkExp.h
@@ -0,0 +1,19 @@
+//
+//  FirstFrameworkExp.h
+//  FirstFrameworkExp
+//
+//  Created by Elizabeth on 29/01/19.
+//  Copyright © 2019 tvisha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirstFrameworkExp.
+FOUNDATION_EXPORT double FirstFrameworkExpVersionNumber;
+
+//! Project version string for FirstFrameworkExp.
+FOUNDATION_EXPORT const unsigned char FirstFrameworkExpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstFrameworkExp/PublicHeader.h>
+
+
